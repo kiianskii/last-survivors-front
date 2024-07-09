201c792bdcb3130da3e2af11759f91810d73a81b
Necrotic999 <l.ilya041105@gmail.com>
2024-07-09 14:40:34 +0300

    create operations and slice for cards

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { cardsReducer } from "./cards/slice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cards: cardsReducer,
+  },
 });
