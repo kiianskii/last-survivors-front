3372efbddcb7b6b93f89e18fd642b20a83402fd1
kiianskii <ankiyanskiy@gmail.com>
2024-07-15 21:20:18 +0300

    added profile btn

--- src/pages/ScreensPage/ScreensPage.jsx
+++ src/pages/ScreensPage/ScreensPage.jsx
@@ -1,13 +1,23 @@
 import AddAnotherCard from "../../components/AddAnotherCard/AddAnotherCard";
 import AddColumnBtn from "../../components/AddColumnBtn/AddColumnBtn";
 import CardsList from "../../components/CardsList/CardsList";
+import EditUserForm from "../../components/EditUserForm/EditUserForm";
+import Modal from "../../components/Modal/Modal";
+import { useToggle } from "../../hooks/useToggle";
 
 function ScreensPage() {
+  const { openModal, closeModal, isOpen } = useToggle();
   return (
     <>
       <CardsList />
       <AddAnotherCard />
       <AddColumnBtn />
+      <button onClick={openModal}>Update user</button>
+      {isOpen && (
+        <Modal title="Edit profile" closeModal={closeModal}>
+          <EditUserForm closeModal={closeModal} />
+        </Modal>
+      )}
     </>
   );
 }
