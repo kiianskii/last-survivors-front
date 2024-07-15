7afb0f4bff9d36250f3716a77f4f94c5f400a432
Kateryna02 <150830031+Kateryna02@users.noreply.github.com>
2024-07-15 21:54:02 +0200

    editAvatar

--- src/components/EditUserForm/EditAvatar.jsx
+++ src/components/EditUserForm/EditAvatar.jsx
@@ -1,5 +1,46 @@
+
+
+
+import React, { useState } from 'react';
+import { Icon } from "../../icons/Icon"; 
+import s from "./EditAvatar.module.css";
+
 function EditAvatar() {
-  return <div>EditAvatar</div>;
+  const [image, setImage] = useState(null);
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div className={s.avatarContainer}>
+      <div className={s.imageContainer}>
+        {image ? (
+          <img src={image} alt="logo" className={s.avatarImage} />
+        ) : (
+          <Icon size={80} id="logo" />
+        )}
+      </div>
+      <input
+        type="file"
+        id="imageUpload"
+        accept="image/*"
+        onChange={handleImageUpload}
+        className={s.fileInput}
+      />
+      <label htmlFor="imageUpload" className={s.uploadButton}>
+        <Icon size={10} id="plus" className={s.icon}/> 
+      </label>
+    </div>
+  );
 }
 
 export default EditAvatar;
+
