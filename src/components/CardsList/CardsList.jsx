cb4ec032483d24d0d52f68a86faf0f56c75b5bdf
Necrotic999 <l.ilya041105@gmail.com>
2024-07-17 20:17:30 +0300

    add cardList

--- src/components/CardsList/CardsList.jsx
+++ src/components/CardsList/CardsList.jsx
@@ -1,10 +1,7 @@
-import { useSelector } from "react-redux";
-import { selectCards } from "../../redux/cards/slice";
 import CardItem from "../CardItem/CardItem";
 import css from "./CardsList.module.css";
 
-const CardsList = () => {
-  const cards = useSelector(selectCards);
+const CardsList = ({ cards }) => {
   return (
     <ul className={css.card_list}>
       {cards.map((card) => {
