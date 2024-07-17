627d2a6d6602416b778de97ded31025879cfc374
kiianskii <ankiyanskiy@gmail.com>
2024-07-17 15:35:09 +0300

    fixed delete

--- src/components/Sidebar/BoardItem/BoardItem.jsx
+++ src/components/Sidebar/BoardItem/BoardItem.jsx
@@ -1,15 +1,18 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Icon } from "../../../icons/Icon";
 import css from "./BoardItem.module.css";
 import { deleteBoard } from "../../../redux/boards/operations";
 import Modal from "../../Modal/Modal";
 import EditBoardForm from "../EditBoardForm/EditBoardForm";
 import { useToggle } from "../../../hooks/useToggle";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { boardsSelector } from "../../../redux/boards/slice";
 
 const BoardItem = ({ board }) => {
   const { openModal, isOpen, closeModal } = useToggle();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const boards = useSelector(boardsSelector);
   return (
     <li className={css.board_item}>
       <Icon size={18} id={board.icon_name} className={css.icons} />
@@ -33,7 +36,10 @@ const BoardItem = ({ board }) => {
           <button
             type="button"
             className={css.button_e}
-            onClick={() => dispatch(deleteBoard({ _id: board._id }))}
+            onClick={() => {
+              dispatch(deleteBoard({ _id: board._id }));
+              navigate(`/${boards[0]._id}`);
+            }}
           >
             <Icon size={16} id="trash" className={css.pen_icon} />
           </button>
