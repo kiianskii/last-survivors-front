9ee3e13c6b240be72429fe3ef48cb43458c3a70d
Nadia <nadiazlagoda@gmail.com>
2024-07-20 18:37:58 +0300

    header, CustomSelect, default button styles remove, authForm styles fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,44 @@
-import BurgerMenu from "../Sidebar/BurgerMenu.jsx/BurgerMenu";
+import { useSelector } from "react-redux";
+import BurgerMenu from "../Sidebar/BurgerMenu.jsx/BurgerMenu.jsx";
 import css from "./Header.module.css";
+import { selectUser } from "../../redux/auth/authSlice.js";
+import CustomSelect from "../Select/CustomSelect.jsx";
+import { themeThunk } from "../../redux/auth/operations";
+import clsx from "clsx";
+
 function Header({ toggleSidebar }) {
-  return (
-    <div className={css.header}>
-      <BurgerMenu toggleSidebar={toggleSidebar} />
-    </div>
-  );
+    // Temp spread and avatarURL and theme
+    const themes = ["dark", "light", "violet"];
+    const userInfo = { ...useSelector(selectUser) };
+    userInfo.avatarURL =
+        "http://res.cloudinary.com/dmcbkckow/image/upload/v1720970849/avatars/spjg2kpl8qvf2pcfyide.webp";
+    userInfo.theme = "light";
+    console.log(userInfo);
+    return (
+        <div className={clsx(css.header, userInfo.theme + "-header")}>
+            <BurgerMenu toggleSidebar={toggleSidebar} />
+            <CustomSelect
+                name="themes"
+                values={themes}
+                placeholder="Theme"
+                currentTheme={userInfo.theme}
+                dispatchFunction={themeThunk}
+            />
+
+            {
+                //TODO Open edit user modal in onclick
+            }
+            <button className={css.profile} onClick={0}>
+                <p className={css.text}>{userInfo.name}</p>
+
+                <img
+                    className={css.img}
+                    src={userInfo.avatarURL}
+                    alt="User Avatar"
+                />
+            </button>
+        </div>
+    );
 }
 
 export default Header;
