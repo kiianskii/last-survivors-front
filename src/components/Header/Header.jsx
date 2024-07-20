58c5597b6031bac8bfc9f12dd5d01235a76e4a67
kiianskii <ankiyanskiy@gmail.com>
2024-07-20 19:10:41 +0300

    added edit modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-
 import { useSelector } from "react-redux";
 import BurgerMenu from "../Sidebar/BurgerMenu.jsx/BurgerMenu.jsx";
 import css from "./Header.module.css";
@@ -6,40 +5,46 @@ import { selectUser } from "../../redux/auth/authSlice.js";
 import CustomSelect from "../Select/CustomSelect.jsx";
 import { themeThunk } from "../../redux/auth/operations";
 import clsx from "clsx";
+import { useToggle } from "../../hooks/useToggle.jsx";
+import EditUserForm from "../EditUserForm/EditUserForm.jsx";
+import Modal from "../Modal/Modal.jsx";
 
 function Header({ toggleSidebar }) {
-    // Temp spread and avatarURL and theme
-    const themes = ["dark", "light", "violet"];
-    const userInfo = { ...useSelector(selectUser) };
-    //userInfo.avatarURL =
-    //    "http://res.cloudinary.com/dmcbkckow/image/upload/v1720970849/avatars/spjg2kpl8qvf2pcfyide.webp";
-    //userInfo.theme = "light";
-    //console.log(userInfo);
-    return (
-        <div className={clsx(css.header, userInfo.theme + "-header")}>
-            <BurgerMenu toggleSidebar={toggleSidebar} />
-            <CustomSelect
-                name="themes"
-                values={themes}
-                placeholder="Theme"
-                currentTheme={userInfo.theme}
-                dispatchFunction={themeThunk}
-            />
+  // Temp spread and avatarURL and theme
+  const themes = ["dark", "light", "violet"];
+  const userInfo = { ...useSelector(selectUser) };
+  //userInfo.avatarURL =
+  //    "http://res.cloudinary.com/dmcbkckow/image/upload/v1720970849/avatars/spjg2kpl8qvf2pcfyide.webp";
+  //userInfo.theme = "light";
+  //console.log(userInfo);
+  const { openModal, closeModal, isOpen } = useToggle();
+  const classname = "class";
+  return (
+    <div className={clsx(css.header, userInfo.theme + "-header")}>
+      <BurgerMenu toggleSidebar={toggleSidebar} />
+      <CustomSelect
+        name="themes"
+        values={themes}
+        placeholder="Theme"
+        currentTheme={userInfo.theme}
+        dispatchFunction={themeThunk}
+      />
+      {isOpen && (
+        <Modal
+          title="Edit profile"
+          closeModal={closeModal}
+          classname={classname}
+        >
+          <EditUserForm closeModal={closeModal} />
+        </Modal>
+      )}
+      <button className={css.profile} onClick={openModal}>
+        <p className={css.text}>{userInfo.name}</p>
 
-            {
-                //TODO Open edit user modal in onclick
-            }
-            <button className={css.profile} onClick={0}>
-                <p className={css.text}>{userInfo.name}</p>
-
-                <img
-                    className={css.img}
-                    src={userInfo.avatarURL}
-                    alt="User Avatar"
-                />
-            </button>
-        </div>
-    );
+        <img className={css.img} src={userInfo.avatarURL} alt="User Avatar" />
+      </button>
+    </div>
+  );
 }
 
 export default Header;
--- src/pages/ScreensPage/ScreensPage.jsx
+++ src/pages/ScreensPage/ScreensPage.jsx
@@ -1,8 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
 import AddColumnBtn from "../../components/AddColumnBtn/AddColumnBtn";
-import EditUserForm from "../../components/EditUserForm/EditUserForm";
-import Modal from "../../components/Modal/Modal";
-import { useToggle } from "../../hooks/useToggle";
 import { selectColumns } from "../../redux/boardByID/slice";
 import ColumnList from "../../components/ColumnList/ColumnList";
 import { useParams } from "react-router-dom";
@@ -12,7 +9,6 @@ import { boardsSelector } from "../../redux/boards/slice";
 import s from "./ScreensPage.module.css";
 
 function ScreensPage() {
-  const { openModal, closeModal, isOpen } = useToggle();
   const boards = useSelector(boardsSelector);
   const dispatch = useDispatch();
   const { boardId } = useParams();
@@ -32,7 +28,7 @@ function ScreensPage() {
   }, [dispatch, boardId, board]);
 
   const columns = useSelector(selectColumns);
-  const classname = "class";
+
   return (
     <div>
       <div className={s.wrapper}>
@@ -43,15 +39,8 @@ function ScreensPage() {
           : ""}
         <AddColumnBtn />
       </div>
-      <button onClick={openModal}>Update user</button>
-      {isOpen && (
-        <Modal title="Edit profile" closeModal={closeModal} classname={classname}>
-          <EditUserForm closeModal={closeModal} />
-        </Modal>
-      )}
     </div>
   );
 }
 
 export default ScreensPage;
-
