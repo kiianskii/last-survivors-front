0213acd5aa41bdfaf52ec5fd4725e7cd54c8457b
kiianskii <ankiyanskiy@gmail.com>
2024-07-21 16:36:35 +0300

    Fixed delete last board

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,21 +1,10 @@
-import { useSelector } from "react-redux";
-import { boardsSelector } from "../../redux/boards/slice";
-import { useNavigate } from "react-router-dom";
 import { useToggle } from "../../hooks/useToggle";
 import Modal from "../Modal/Modal";
 import AddBoardForm from "../Sidebar/AddBoardForm/AddBoardForm";
 import css from "./Dashboard.module.css";
-import { useEffect } from "react";
 
 function Dashboard() {
   const { isOpen, closeModal, openModal } = useToggle();
-  const boards = useSelector(boardsSelector);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (boards.length) {
-      return navigate(`/${boards[0]._id}`);
-    }
-  }, [boards, navigate]);
 
   return (
     <div>
