a17c97fc2ebcb6b78b6ec5ae269573eaa5189426
Karolina Zaiats <karolina.ivanitska@gmail.com>
2024-07-21 17:50:29 +0200

    Add margin and color text to dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -10,7 +10,9 @@ function Dashboard() {
     <div>
       <h3 className={css.wrapper}>
         Before starting your project, it is essential
-        <span onClick={openModal}>to create a board</span>
+        <span onClick={openModal} className={css.span}>
+          to create a board
+        </span>
         to visualize and track all the necessary tasks and milestones. This
         board serves as a powerful tool to organize the workflow and ensure
         effective collaboration among team members.
