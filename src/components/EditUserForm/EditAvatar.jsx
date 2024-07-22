1e18bfae604d0a1118f40d58cdadeb191d02d462
kiianskii <ankiyanskiy@gmail.com>
2024-07-22 14:46:04 +0300

    fixed styles

--- src/components/EditUserForm/EditAvatar.jsx
+++ src/components/EditUserForm/EditAvatar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectTheme, selectAvatar } from "../../redux/auth/authSlice";
 import { editAvatarThunk } from "../../redux/auth/operations";
